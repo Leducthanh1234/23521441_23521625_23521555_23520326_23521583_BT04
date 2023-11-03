12f3c86f1a0668b28915e80ff73d33b97c221816
phamtranxuan-tien <23521583@gm.uit.edu.vn>
2023-11-03 14:41:02 +0700

    tien

---
+++ Bai071/Bai071.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Nhap(float[][100], int&, int&);
+void Xuat(float[][100], int, int);
+
+int DemCucTieu(float[][100], int, int);
+int ktCucTieu(float[][100], int, int, int, int);
+int DemCucDai(float[][100], int, int);
+int ktCucDai(float[][100], int, int, int, int);
+
+int main()
+{
+
+	float b[100][100];
+	int k, l;
+	cout << "Ma tran: \n";
+	Nhap(b, k, l);
+
+	cout << "Ma tran ban dau:\n";
+	Xuat(b, k, l);
+
+	cout << "So luong phan tu cuc tieu: " << DemCucTieu(b, k, l);
+	int kq = DemCucDai(b, k, l);
+	cout << "\nSo luong phan tu cuc dai la: " << kq;
+	cout << "\n\n\nKet Thuc!!!";
+	return 0;
+}
+
+void Nhap(float a[][100], int& m, int& n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so cot: ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			a[i][j] = -100 + rand() / ((float)RAND_MAX / 200);
+}
+
+void Xuat(float a[][100], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << fixed << setw(10) << setprecision(3) << a[i][j];
+		cout << endl;
+	}
+}
+
+int DemCucTieu(float a[][100], int m, int n)
+{
+	int dem = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (ktCucTieu(a, m, n, i, j))
+				dem++;
+	return dem;
+}
+
+int ktCucTieu(float a[][100], int m, int n, int dd, int cc)
+{
+	int di[8] = { -1,-1,-1,0,+1,+1,+1,0 };
+	int dj[8] = { -1,0,+1,+1,+1,0,-1,-1 };
+	int flag = 1;
+	for (int k = 0; k < 8; k++)
+		if (dd + di[k] >= 0 && dd + di[k] < n && cc + dj[k] >= 0 && cc + dj[k] < m)
+			if (a[dd + di[k]][cc + dj[k]] > a[dd][cc])
+				flag = 0;
+	return flag;
+}
+
+int DemCucDai(float a[][100], int m, int n)
+{
+	int dem = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (ktCucDai(a, m, n, i, j) == 1)
+				dem++;
+	return dem;
+}
+
+int ktCucDai(float a[][100], int m, int n, int d, int c)
+{
+	int di[8] = { -1,-1,-1,0,+1,+1,+1,0 };
+	int dj[8] = { -1,0,+1,+1,+1,0,-1,-1 };
+	int flag = 1;
+	for (int k = 0; k < 8; k++)
+		if (d + di[k] >= 0 && d + di[k] < m && c + dj[k] >= 0 && c + dj[k] < n)
+			if (a[d + di[k]][c + dj[k]] > a[d][c])
+				flag = 0;
+	return flag;
+}
